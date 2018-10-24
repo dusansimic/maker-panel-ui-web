65d1d5da9300882e35342b58b5c4f24d2fab0b9b
Dušan Simić <dusan.simic1810@gmail.com>
2018-10-24 13:59:28 +0200

    Implementing elements
    Implementing elements button and storing elements list into indexeddb.
    Need to implement differentiating elements by appid and deviceid.

--- src/views/Device.vue
+++ src/views/Device.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
-    <!-- <LineChart v-if="this.temperature" :chart-data="{labels: this.time, datasets: [{label: 'Temperature Label', backgroundColor: '#3f89ff', data: this.temperature}]}" :options="this.temperatureOptions"/> -->
-    <b-button variant=secondary class="w-100" v-b-modal.add-element-modal>+</b-button>
+    <b-card v-for="element in elementList" :key="element.name" class="chart-card">
+      <p class="card-text">
+        <b-dropdown size=sm text="Options" variant=light>
+          <b-dropdown-item-button @click="deleteChart(element.name)">Delete</b-dropdown-item-button>
+        </b-dropdown>
+
+        <LineChart v-if="element.type === 'Line chart'" :chart-data="{labels: sourceTimes, datasets: [{label: element.name, data: sourceData[element.dataSource], backgroundColor: element.color}]}" :options="chartOptions"></LineChart>
+        <BarChart v-if="element.type === 'Bar chart'" :chart-data="{labels: sourceTimes, datasets: [{label: element.name, data: sourceData[element.dataSource], backgroundColor: element.color}]}" :options="chartOptions"></BarChart>
+      </p>
+    </b-card>
+    <b-button variant=secondary class="w-100" v-b-modal.add-element-modal id="add-button">Add element</b-button>
 
     <b-modal id=add-element-modal title="Add element" @ok=onModalOk>
       <b-form>
@@ -45,6 +54,7 @@
 
 <script>
 import LineChart from '@/components/LineChart'
+import BarChart from '@/components/BarChart'
 import axios from 'axios'
 import moment from 'moment'
 import Dexie from 'dexie'
@@ -52,11 +62,12 @@ import Dexie from 'dexie'
 export default {
   name: 'Device',
   components: {
-    LineChart
+    LineChart,
+    BarChart
   },
   data () {
     return {
-      time: [],
+      sourceTimes: [],
       db: null,
       newElementData: {
         name: '',
@@ -68,7 +79,9 @@ export default {
         'Bar chart'
       ],
       elementSourceDataOptions: [],
-      sourceData: {}
+      sourceData: {},
+      elementList: [],
+      chartOptions: {responsive: true, maintainAspectRatio: false}
     }
   },
   methods: {
@@ -87,17 +100,44 @@ export default {
       for (const source of this.elementSourceDataOptions) {
         this.sourceData[source] = data.map(obj => obj.payload_fields[source])
       }
-      this.sourceTimes = data.map(obj => obj.metadata.time)
+      this.sourceTimes = data.map(obj => moment(obj.metadata.time).format('hh:mm'))
     },
     getTimeInFormat (time, format) {
       return moment(time).format(format)
     },
     async onModalOk () {
-      this.db.elements.put({
+      await this.db.elements.add({
         name: this.newElementData.name,
+        appid: this.$route.params.applicationId,
+        deviceid: this.$route.params.deviceId,
         type: this.newElementData.type,
-        dataSource: this.newElementData.dataSource
+        dataSource: this.newElementData.dataSource,
+        color: this.randomHEX()
       })
+
+      this.elementList = await this.getElements()
+
+      this.newElementData.name = ''
+      this.newElementData.type = ''
+      this.newElementData.dataSource = ''
+    },
+    randomHEX () {
+      return `#${(Math.random() * 0xFFFFFF << 0).toString(16)}`
+    },
+    async deleteChart (name) {
+      await this.db.elements.where({
+        appid: this.$route.params.applicationId,
+        deviceid: this.$route.params.deviceId,
+        name
+      }).delete()
+
+      this.elementList = await this.getElements()
+    },
+    async getElements () {
+      return this.db.elements.where({
+        appid: this.$route.params.applicationId,
+        deviceid: this.$route.params.deviceId
+      }).toArray()
     }
   },
   async mounted () {
@@ -105,8 +145,19 @@ export default {
 
     this.db = new Dexie('device_display')
     this.db.version(1).stores({
-      elements: 'name,type,dataSource'
+      elements: 'name,appid,deviceid,type,dataSource,color'
     })
+
+    this.elementList = await this.db.elements.toArray()
   }
 }
 </script>
+
+<style>
+.chart-card {
+  margin-top: 10px;
+}
+#add-button {
+  margin-top: 10px;
+}
+</style>
