df18276c2bc2e1251a3b9edd3578a9a5b559ebf7
Dušan Simić <dusan.simic1810@gmail.com>
2018-10-24 17:30:40 +0200

    Finished implementing elements and fixed css bug
    Implementation of widgets is finished and fully functional. All elements
    are stored in indexeddb. Also added small css bug fix in footer.

--- src/App.vue
+++ src/App.vue
@@ -123,6 +123,8 @@ footer {
   height: 100px;
 }
 footer .aligner {
+  width: 100%;
+  height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
--- src/views/Device.vue
+++ src/views/Device.vue
@@ -2,12 +2,10 @@
   <div>
     <b-card v-for="element in elementList" :key="element.name" class="chart-card">
       <p class="card-text">
-        <b-dropdown size=sm text="Options" variant=light>
-          <b-dropdown-item-button @click="deleteChart(element.name)">Delete</b-dropdown-item-button>
-        </b-dropdown>
+        <b-button variant=danger size=sm @click="deleteChart(element.id)" class="float-right">Delete</b-button>
 
-        <LineChart v-if="element.type === 'Line chart'" :chart-data="{labels: sourceTimes, datasets: [{label: element.name, data: sourceData[element.dataSource], backgroundColor: element.color}]}" :options="chartOptions"></LineChart>
-        <BarChart v-if="element.type === 'Bar chart'" :chart-data="{labels: sourceTimes, datasets: [{label: element.name, data: sourceData[element.dataSource], backgroundColor: element.color}]}" :options="chartOptions"></BarChart>
+        <LineChart v-if="element.type === 'Line chart'" :chart-data="{labels: sourceTimes, datasets: [{label: element.name, data: sourceData[element.dataSource], backgroundColor: element.color}]}" :options="chartOptions" :height="350"></LineChart>
+        <BarChart v-if="element.type === 'Bar chart'" :chart-data="{labels: sourceTimes, datasets: [{label: element.name, data: sourceData[element.dataSource], backgroundColor: element.color}]}" :options="chartOptions" :height="350"></BarChart>
       </p>
     </b-card>
     <b-button variant=secondary class="w-100" v-b-modal.add-element-modal id="add-button">Add element</b-button>
@@ -81,17 +79,24 @@ export default {
       elementSourceDataOptions: [],
       sourceData: {},
       elementList: [],
-      chartOptions: {responsive: true, maintainAspectRatio: false}
+      chartOptions: {responsive: true},
+      ids: {
+        application: this.$route.params.applicationId,
+        device: this.$route.params.deviceId
+      }
     }
   },
   methods: {
-    async fetchData () {
+    async fetchData (removeLast) {
       // Create requrest and get data
       let currentDate = new Date()
       let minimalDate = moment(currentDate).subtract(30, 'minutes').utcOffset('+00:00').toISOString()
-      const {data, status, statusText} = await axios.get(`https://maker-panel-backend.herokuapp.com/api/rest/${this.$route.params.applicationId}/device/${this.$route.params.deviceId}?time=${minimalDate}`)
+      const {data, status, statusText} = await axios.get(`https://maker-panel-backend.herokuapp.com/api/rest/${this.ids.application}/device/${this.ids.device}?time=${minimalDate}`)
       if (status > 399) {
         return alert(statusText)
+      }
+      if (data.length === 0) {
+        return alert('No data was provided!')
       }
 
       // Get source options
@@ -99,24 +104,29 @@ export default {
       // Map data into different arrays
       for (const source of this.elementSourceDataOptions) {
         this.sourceData[source] = data.map(obj => obj.payload_fields[source])
+        if (removeLast) this.sourceData[source].splice(0, 1)
       }
-      this.sourceTimes = data.map(obj => moment(obj.metadata.time).format('hh:mm'))
+      this.sourceTimes = data.map(obj => moment(obj.metadata.time).format('HH:mm'))
+      if (removeLast) this.sourceTimes.splice(0, 1)
     },
     getTimeInFormat (time, format) {
       return moment(time).format(format)
     },
     async onModalOk () {
+      // Add data to table
       await this.db.elements.add({
         name: this.newElementData.name,
-        appid: this.$route.params.applicationId,
-        deviceid: this.$route.params.deviceId,
+        appid: this.ids.application,
+        deviceid: this.ids.device,
         type: this.newElementData.type,
         dataSource: this.newElementData.dataSource,
         color: this.randomHEX()
       })
 
+      // Refresh elements list
       this.elementList = await this.getElements()
 
+      // Clear data input
       this.newElementData.name = ''
       this.newElementData.type = ''
       this.newElementData.dataSource = ''
@@ -124,31 +134,38 @@ export default {
     randomHEX () {
       return `#${(Math.random() * 0xFFFFFF << 0).toString(16)}`
     },
-    async deleteChart (name) {
-      await this.db.elements.where({
-        appid: this.$route.params.applicationId,
-        deviceid: this.$route.params.deviceId,
-        name
-      }).delete()
+    async deleteChart (id) {
+      // Delete element with specified id
+      await this.db.elements.where('id').equals(id).delete()
 
+      // Update element list
       this.elementList = await this.getElements()
     },
     async getElements () {
+      // Get elements from table
+      // App and device ids need to match current device
       return this.db.elements.where({
-        appid: this.$route.params.applicationId,
-        deviceid: this.$route.params.deviceId
+        appid: this.ids.application,
+        deviceid: this.ids.device
       }).toArray()
     }
   },
   async mounted () {
-    this.fetchData()
+    this.fetchData(true)
+    const self = this
+    setInterval(() => {
+      self.fetchData(true)
+    }, 60000)
 
+    // Create indexeddb connection
     this.db = new Dexie('device_display')
+    // Create table schema
     this.db.version(1).stores({
-      elements: 'name,appid,deviceid,type,dataSource,color'
+      elements: '++id,name,appid,deviceid,type,dataSource,color'
     })
 
-    this.elementList = await this.db.elements.toArray()
+    // Get element that are already added to table
+    this.elementList = await this.getElements()
   }
 }
 </script>
